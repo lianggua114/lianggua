90cc31ef5b952c3ae0db4cb638e03fe4df30a43d
lianggua114 <sli37121@gmail.com>
2026-08-07 14:28:49 +0800

    Add default values for env vars

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://chtnkmrdoanrsnepkoxr.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNodG5rbXJkb2FucnNuZXBrb3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMDg3MzYsImV4cCI6MjEwMTU4NDczNn0.2EUa_yX9AqWbtCaZH3GfHiXZtts_U760v6lM0AcegiU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
